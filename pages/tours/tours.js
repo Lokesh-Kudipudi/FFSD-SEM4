bc13f6d464aa69c83e5e2cd5f503aec703f53c28
Lokesh_Kudipudi <lokeshkudipudi2006@gmail.com>
2025-02-07 18:12:34 +0530

    Adds rating filter

--- pages/tours/tours.js
+++ pages/tours/tours.js
@@ -1,108 +1,163 @@
 const tours = [
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Golden Triangle: Delhi, Agra & Jaipur",
     tags: ["featured"],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 5,
+    duration: "6 days 5 nights",
+    price: "28,000.00",
+    img: "https://triptovaranasi.in/wp-content/uploads/2023/08/Varanasi-1024x664.webp",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
-    tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    title: "Backwaters of Kerala: Alleppey & Kumarakom",
+    tags: ["popular"],
+    rating: 4,
+    duration: "5 days 4 nights",
+    price: "22,500.00",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIbadu9m2jEZPw8VwMwbViQlXerMZ_TTc-hg&s",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
-    tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    title: "Majestic Rajasthan: Udaipur, Jodhpur & Jaisalmer",
+    tags: ["new"],
+    rating: 5,
+    duration: "8 days 7 nights",
+    price: "30,000.00",
+    img: "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvZnJpbmRpYV9jb2tlX3B1ZXJ0YV9tZXJsaW5fMC1pbWFnZS1reWJkZmpqci5qcGc.jpg",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
-    tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    title: "Spiritual Varanasi & Sarnath Tour",
+    tags: ["spiritual"],
+    rating: 5,
+    duration: "4 days 3 nights",
+    price: "15,000.00",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9_VDxOBv8Q1u3yjsZO6aUgbFsaEXL0cb6NA&s",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Kashmir Paradise: Srinagar, Gulmarg & Pahalgam",
     tags: [],
+    rating: 4,
     duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    price: "35,000.00",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTDtqVgKil3J5sQeg_u8xU5FhfXJ52zEps9Q&s",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Goa Beach & Adventure Tour",
     tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 3,
+    duration: "5 days 4 nights",
+    price: "20,500.00",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhwZZXAO5lOnJ5ue3I3mx2KIQ19rmdZs3eWw&s",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Himalayan Escape: Shimla & Manali",
     tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 4,
+    duration: "6 days 5 nights",
+    price: "26,000.00",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaWtgI18oOyQCJpoOIoLuQAbPGYrgT1QkoAQ&s",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Andaman Islands: Port Blair & Havelock",
     tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 4,
+    duration: "6 days 5 nights",
+    price: "32,000.00",
+    img: "https://images.rawpixel.com/image_social_landscape/cHJpdmF0ZS9zdGF0aWMvaW1hZ2Uvd2Vic2l0ZS8yMDIyLTA0L2xyL2ZydGhhaWxhbmRfcGh1a2V0X2tvaF9waGlfOC1pbWFnZS1reWJhaDJpYi5qcGc.jpg",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Darjeeling & Gangtok: Himalayan Retreat",
     tags: [],
+    rating: 4,
     duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    price: "25,500.00",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQspADhqmvbB5NqFX55mw05WhwgO1Z2Iso9qg&s",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Ranthambore & Jaipur: Wildlife & Heritage",
     tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 5,
+    duration: "5 days 4 nights",
+    price: "23,000.00",
+    img: "https://upload.wikimedia.org/wikipedia/commons/1/1f/Naulakha_gate%2Cranthambor_fort.jpg",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Rishikesh & Haridwar Yoga & Spiritual Retreat",
     tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 3,
+    duration: "5 days 4 nights",
+    price: "18,000.00",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIHAHWCb9Sfaa_jKKJ6h7qSKeD-9obvpvn0Q&s",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Meghalaya: Cherrapunji & Shillong Adventure",
     tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 0,
+    duration: "6 days 5 nights",
+    price: "27,000.00",
+    img: "https://images.pexels.com/photos/18476582/pexels-photo-18476582/free-photo-of-waterfalls-on-rocks-in-deep-forest.jpeg",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Madurai, Rameshwaram & Kanyakumari Pilgrimage",
     tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 2,
+    duration: "6 days 5 nights",
+    price: "21,500.00",
+    img: "https://upload.wikimedia.org/wikipedia/commons/b/b5/India_-_Madurai_temple_-_0781.jpg",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Sundarbans Wildlife Safari",
     tags: [],
-    duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    rating: 5,
+    duration: "4 days 3 nights",
+    price: "19,000.00",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/70/Chital_deer%2C_Sundarbans_East_Wildlife_Sanctuary_01.jpg",
   },
   {
-    title: "Cochin, Alleppey, Varkala, & Kovalam Tour",
+    title: "Ooty & Coorg Hill Station Retreat",
     tags: [],
+    rating: 5,
+    duration: "6 days 5 nights",
+    price: "24,500.00",
+    img: "https://live.staticflickr.com/3588/3445424333_f14c80b6da_b.jpg",
+  },
+  {
+    title: "Leh & Ladakh Adventure Tour",
+    tags: [],
+    rating: 5,
     duration: "7 days 6 nights",
-    price: "24,000.00",
-    img: "../../assets/tours/heroBg1.jpg",
+    price: "40,000.00",
+    img: "https://live.staticflickr.com/7289/10991694376_40a1f41791_b.jpg",
+  },
+  {
+    title: "Pondicherry & Mahabalipuram Coastal Tour",
+    tags: [],
+    rating: 5,
+    duration: "5 days 4 nights",
+    price: "19,500.00",
+    img: "https://www.worldhistory.org/uploads/images/4127.jpg",
+  },
+  {
+    title: "Lakshadweep Island Getaway",
+    tags: [],
+    duration: "5 days 4 nights",
+    rating: 5,
+    price: "35,000.00",
+    img: "https://images.pexels.com/photos/22614625/pexels-photo-22614625/free-photo-of-idyllic-beach-on-bangaram-island-in-india.jpeg",
+  },
+  {
+    title: "Bhubaneswar & Puri: Odisha Heritage",
+    tags: [],
+    rating: 5,
+    duration: "5 days 4 nights",
+    price: "21,000.00",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe7Q96iTxbBt6p28T9o3Nl5knojVV7_GRBjQ&s",
+  },
+  {
+    title: "Ajanta & Ellora: Maharashtra Heritage Tour",
+    tags: [],
+    rating: 5,
+    duration: "4 days 3 nights",
+    price: "18,500.00",
+    img: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Ajanta_Ellora_caves-_Maharashtra%2CIndia.jpg",
   },
 ];
 
@@ -111,9 +166,11 @@ const buttonRight = document.querySelector(".buttonRight");
 
 const numberOfCardsPerPage = 12;
 const numberOfPages = Math.floor(
-  tours.length / numberOfCardsPerPage
+  (tours.length - 1) / numberOfCardsPerPage
 );
 let currentPage = 0;
+
+let ratingSelected = [];
 
 // 0 ->
 
@@ -121,16 +178,43 @@ const allToursContainer = document.querySelector(
   ".allToursContainer"
 );
 
-const location = document.querySelector(
+const locationHeader = document.querySelector(
   ".dropdown-header-location"
 );
 
-const rating = document.querySelector(".dropdown-header-rating");
+const ratingHeader = document.querySelector(
+  ".dropdown-header-rating"
+);
 
 function displayToursTemplate() {
+  function generateStars(i) {
+    const singleStar = `<i class="fas fa-star"></i>`;
+    let stars = "";
+    if (!i) {
+      return "No Reviews";
+    }
+    while (i) {
+      stars = stars + singleStar;
+      i--;
+    }
+    return stars;
+  }
+
   allToursContainer.innerHTML = "";
 
-  const toursTemplate = tours
+  let toursToDisplay = tours;
+
+  if (ratingSelected.length != 0) {
+    toursToDisplay = tours.filter((tour) => {
+      if (ratingSelected.includes(tour.rating)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+
+  const toursTemplate = toursToDisplay
     .slice(
       currentPage * numberOfCardsPerPage,
       currentPage * numberOfCardsPerPage + numberOfCardsPerPage
@@ -151,7 +235,7 @@ function displayToursTemplate() {
           <div class="card-content">
             <h3>${tour.title}</h3>
             <div class="rating">
-              <i class="fas fa-star"></i> 0 (No Review)
+              ${generateStars(tour.rating)}
             </div>
             <div class="price">From ₹${tour.price}</div>
             <div class="duration">${tour.duration}</div>
@@ -196,7 +280,15 @@ document
   .getElementById("dropdownContentRating")
   .addEventListener("change", function (event) {
     if (event.target.type === "checkbox") {
-      console.log(event.target.value);
+      if (ratingSelected.includes(+event.target.value)) {
+        ratingSelected = ratingSelected.filter(
+          (num) => num != +event.target.value
+        );
+      } else {
+        ratingSelected.push(+event.target.value);
+      }
+      console.log(ratingSelected);
+      displayToursTemplate();
     }
   });
 
@@ -219,11 +311,11 @@ function toggleDropdown(filter) {
   }
 }
 
-rating.addEventListener("click", () => {
+ratingHeader.addEventListener("click", () => {
   toggleDropdown("Rating");
 });
 
-location.addEventListener("click", () => {
+locationHeader.addEventListener("click", () => {
   toggleDropdown("Location");
 });
 
