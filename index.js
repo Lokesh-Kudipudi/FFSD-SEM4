4916578dd1b0da6ad3aada6b5829f495d5b3f4d1
Lokesh_Kudipudi <lokeshkudipudi2006@gmail.com>
2025-02-27 16:41:29 +0530

    Added MVC folders

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { name: "Testing" });
+});
+
+app.get("/tours", (req, res) => {
+  res.render("tours/index", { name: "Testing" });
+});
+
+const server = app.listen("5500", () => {
+  console.log("Server Started");
+});
